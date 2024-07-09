09483f28e66795666f603da214dea658504961c9
yigitkalan <sametykalan@gmail.com>
2024-07-10 01:50:41 +0300

    create cmp and lsp configs

---
+++ .config/nvim/lua/configs/cmp-config.lua
@@ -0,0 +1,52 @@
+local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+
+-- Configure nvim-cmp
+local cmp_config = lsp_zero.defaults.cmp_config({
+    mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50,
+            ellipsis_char = '...',
+            before = function(entry, vim_item)
+                return vim_item
+            end
+        })
+    },
+    preselect = cmp.PreselectMode.Item,
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    window = {
+        completion = cmp.config.window.bordered({
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+            border = 'rounded',
+        }),
+        documentation = cmp.config.window.bordered({
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+            border = 'rounded',
+        }),
+    },
+})
+
+cmp.setup(cmp_config)
